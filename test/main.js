8d205662da6945bc67a5749b10d08dcd7271164b
skbyeon <cde123123@naver.com>
2018-12-05 15:47:16 +0900

    상품등록 코드 추가...(버튼 안눌리는 이슈 해결 필요함)

--- test/main.js
+++ test/main.js
@@ -20,21 +20,6 @@ module.exports = app => {
     app.post('/myaction', urlencodedParser, (req, res) => {
     //console.log(req.body.name + ' in myaction');
 
-    // 옵션
-    // 선택형 옵션 chkSelectOption
-    if(req.body.chkSelectOption != undefined)
-    {
-        // radOptionType(radOptionTypeSolo, radOptionTypeMix) 선택형 옵션 유형
-        // selSelectOptionNum(1,2,3) 옵션명 개수
-        // selSelectOptionOrder(orderRegistr, orderAbc, orderHighPrice, orderLowPrice) 정렬순서
-    }
-
-    // 직접입력형 옵션 chkSelectOption
-    if(req.body.chkInputOption != undefined)
-    {
-        // selInputOptionNum 옵션명 개수
-    }
-
     // 상세설명
     // tProductDescription
 
@@ -106,16 +91,14 @@ module.exports = app => {
 
     console.log(req.body.name + ' in myaction');
     await page.click('#loginButton');
-    await page.screenshot({path: 'example1.png'});
     //await page.waitForNavigation( { waitUntil : 'networkidle2' } );
     await page.waitForSelector('#seller-lnb');
 
     // 스토어 홈
-    await page.screenshot({path: 'example2.png'});
     await page.click('#seller-lnb > div > div:nth-child(1) > ul > li:nth-child(1) > a');
 
     // 상품등록 메뉴 클릭
-    await page.waitForSelector('#seller-lnb > div > div:nth-child(1) > ul > li.ng-scope.active > ul > li:nth-child(1) > a');
+    await page.waitForSelector('#seller-lnb > div > div:nth-child(1) > ul > li.ng-scope.active > ul > li:nth-child(2) > a');
     await page.click('#seller-lnb > div > div:nth-child(1) > ul > li.ng-scope.active > ul > li:nth-child(2) > a');
 
     // 상품명
@@ -127,31 +110,103 @@ module.exports = app => {
     await page.waitForSelector('input[name="product.salePrice"]', { timeout: 3000 });
     await page.type('input[name="product.salePrice"]', req.body.nProductPrice);
     console.log('1');
-    await page.waitForSelector('#' + req.body.selProductTax, { timeout: 3000 });
+    //await page.waitForSelector('#' + req.body.selProductTax, { timeout: 3000 });
     console.log('#' + req.body.selProductTax);
-    await page.click('#' + req.body.selProductTax); // 부가세
-    console.log('3');
+    //await page.click('#' + req.body.selProductTax); // 부가세
+    await page.click('input[value="SMALL"]');
+    console.log('input[value="SMALL"]');
     if(req.body.chkProductSale != undefined) // 할인설정
     {
         await page.click('#r3_1_total');
         await page.waitForSelector('input[name="product.customerBenefit.immediateDiscountPolicy.discountMethod.value"]', { timeout: 3000 });
-        await page.type('input[name="product.customerBenefit.immediateDiscountPolicy.discountMethod.value"]', req.body.nProductPrice);
+        await page.type('input[name="product.customerBenefit.immediateDiscountPolicy.discountMethod.value"]', req.body.nSaleValue);
+
+        console.log('할인 : ' + req.body.nSaleValue);
 
         await page.click('.caret'); // 할인 단위
-        var nIdex = (req.body.selSaleType == 'won' ? 1 : 2);
+        var nIdex = (req.body.selSaleType == 'won' ? 2 : 1);
         await page.click('#error_immediateDiscountPolicy_all_value > div:nth-child(1) > div > div.input-group-btn.open > ul > li:nth-child('+nIdex+')');
     }
-    await page.waitFor(3000);
 
+    // 재고 수량
+    await page.type('#stock', '30'); // 재고수량 30 고정
+
+    console.log(req.body.chkSelectOption);
+    console.log(req.body.chkInputOption);
+
+        // 옵션
+    if(req.body.chkSelectOption != undefined
+       || req.body.chkInputOption != undefined)
+    {
+        // 옵션 영역 펼치기
+        await page.waitForSelector('div[server-field-errors="product.detailAttribute.optionInfo.*"]', { timeout: 3000 });
+        await page.click('div[server-field-errors="product.detailAttribute.optionInfo.*"]');
+         console.log("logOption2");
+    }
+
+    // 선택형 옵션 chkSelectOption
+    if(req.body.chkSelectOption != undefined)
+    {
+        console.log("select logOption1");
+        // 선택형 옵션 펼치기
+        await page.click('#option_choice_type_true');
+
+        // radOptionType(radOptionTypeSolo, radOptionTypeMix) 선택형 옵션 유형
+
+        console.log(req.body.radOptionType);
+        if(req.body.radOptionType == 'radOptionTypeSolo')
+            await page.click('input[value="SIMPLE"]'); // 단독형
+        else
+            await page.click('input[value="COMBINATION"]'); // 조합형
+
+        console.log(req.body.selSelectOptionNum);
+        // selSelectOptionNum(1,2,3) 옵션명 개수
+        page.evaluate(async()=>{
+            await Promise.all([
+                page.waitForSelector('#productForm > ng-include > ui-view:nth-child(8) > div > fieldset > div > div > div:nth-child(2) > div.form-sub-detail-wrap.ng-scope > div:nth-child(2) > div > div > div > div > div.selectize-input.items.ng-valid.ng-pristine.has-options.full.has-items');
+                page.click('#productForm > ng-include > ui-view:nth-child(8) > div > fieldset > div > div > div:nth-child(2) > div.form-sub-detail-wrap.ng-scope > div:nth-child(2) > div > div > div > div > div.selectize-input.items.ng-valid.ng-pristine.has-options.full.has-items');
+            ]);
+        });
+
+//        await page.click('div[data-value="' + req.body.selSelectOptionNum + '"]')
+
+
+//        console.log(req.body.selSelectOptionOrder);
+//        // selSelectOptionOrder(orderRegistr, orderAbc, orderHighPrice, orderLowPrice) 정렬순서
+//        switch(req.body.selSelectOptionOrder)
+//        {
+//            case 'orderRegistr' :
+//                await page.click('div[data-value="CREATE"]');
+//                break;
+//            case 'orderAbc' :
+//                await page.click('div[data-value="ABC"]');
+//                break;
+//             case 'orderLowPrice' :
+//                await page.click('div[data-value="LOW_PRICE"]');
+//                break;
+//            case 'ordeorderHighPricer' :
+//                await page.click('div[data-value="HIGH_PRICE"]');
+//                break;
+//            default :
+//                console.log("선택형 옵션 정렬순서 오류");
+//                break;
+//        }
+    }
+
+    // 직접입력형 옵션 chkSelectOption
+    if(req.body.chkInputOption != undefined)
+    {
+        // selInputOptionNum 옵션명 개수
+    }
 
     // 상품이미지 - 대표이미지
-    await page.waitForSelector('#representImage > div > div.seller-product-img.add-img > div > ul > li > div > a');
-    await page.click('#representImage > div > div.seller-product-img.add-img > div > ul > li > div > a');
+    //await page.waitForSelector('#representImage > div > div.seller-product-img.add-img > div > ul > li > div > a');
+    //await page.click('#representImage > div > div.seller-product-img.add-img > div > ul > li > div > a');
 
     // 상품이미지 - 내사진 - file upload
-    await page.waitForSelector('input[type="file"]', { timeout: 3000 });
-    const input = await page.$('input[type="file"]');
-    await input.uploadFile(__dirname + '\\..\\images\\product_image.jpg');
+   // await page.waitForSelector('input[type="file"]', { timeout: 3000 });
+   // const input = await page.$('input[type="file"]');
+    //await input.uploadFile(__dirname + '\\..\\images\\product_image.jpg');
 
     // 상세설명
 //    await page.waitForSelector('[class="btn btn-primary btn-lg ng-binding"]');
@@ -166,11 +221,9 @@ module.exports = app => {
 //    await popup.click('[title=구분선]');
 //    await popup.click('#se_top_publish_btn');
 
-    await page.waitFor(1000);
+    await page.waitFor(10000);
 
-    await page.screenshot({path: 'example3.png'});
-
-    res.send('Product registration succeeded..!');
+    //res.send('Product registration succeeded..!');
 
     await page.evaluate(() => console.log(`url is ${location.href}`));
     browser.close();
